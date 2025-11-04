db0f7cb063a149027142b9557651935ea6d5b72b
thetrueseros <leugimdzn@hotmail.com>
2025-11-04 14:48:15 -0500

    Implementación Clase CursoPresencial

---
+++ Sistema Gestion Cursos/CursoPresencial.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Gestion_Cursos
+{
+    internal class CursoPresencial
+    {
+    }
+}
