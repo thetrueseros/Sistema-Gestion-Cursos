a27c02d0bdd59d92a3d5239e5087c8a92e1889df
thetrueseros <leugimdzn@hotmail.com>
2025-11-04 14:40:40 -0500

    Creación clase abstracta Curso

---
+++ Sistema Gestion Cursos/Curso.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Gestion_Cursos
+{
+    public abstract class Curso
+    {
+        public string nombreCurso { get; set; }
+        public string docente { get; set; }
+        public string modalidad { get; set; }
+        public double intensidadHoraria { get; set; }
+        public int cantidadEstudiantes { get; set; }
+        public double precioUnit { get; set; }
+
+        public Curso(string nombreCurso, string docente, string modalidad, double intensidadHoraria, int cantidadEstudiantes, double precioUnit)
+        {
+            this.nombreCurso = nombreCurso;
+            this.docente = docente;
+            this.modalidad = modalidad;
+            this.intensidadHoraria = intensidadHoraria;
+            this.cantidadEstudiantes = cantidadEstudiantes;
+            this.precioUnit = precioUnit;
+        }
+
+        public abstract double CalcularPrecioFinal();
+    }
+}
