9bac2d779f77254fef930500673b967b72585d89
thetrueseros <leugimdzn@hotmail.com>
2025-11-04 14:46:18 -0500

    Implementación clase hija "Virtual" (curso)

--- Sistema Gestion Cursos/Curso.cs
+++ Sistema Gestion Cursos/Curso.cs
@@ -10,21 +10,19 @@ namespace Sistema_Gestion_Cursos
     {
         public string nombreCurso { get; set; }
         public string docente { get; set; }
-        public string modalidad { get; set; }
         public double intensidadHoraria { get; set; }
         public int cantidadEstudiantes { get; set; }
         public double precioUnit { get; set; }
 
-        public Curso(string nombreCurso, string docente, string modalidad, double intensidadHoraria, int cantidadEstudiantes, double precioUnit)
+        public Curso(string nombreCurso, string docente, double intensidadHoraria, int cantidadEstudiantes, double precioUnit)
         {
             this.nombreCurso = nombreCurso;
             this.docente = docente;
-            this.modalidad = modalidad;
             this.intensidadHoraria = intensidadHoraria;
             this.cantidadEstudiantes = cantidadEstudiantes;
             this.precioUnit = precioUnit;
         }
 
-        public abstract double CalcularPrecioFinal();
+        public abstract double CalcularPrecioFinal(int cantidadEstudiantes, double precioUnit);
     }
 }
---
+++ Sistema Gestion Cursos/Virtual.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_Gestion_Cursos
+{
+    public class Virtual : Curso
+    {
+        public Virtual(string nombreCurso, string docente, double intensidadHoraria, int cantidadEstudiantes, double precioUnit)
+            : base(nombreCurso, docente, intensidadHoraria, cantidadEstudiantes, precioUnit)
+        {
+        }
+        public override double CalcularPrecioFinal(int cantidadEstudiantes, double precioUnit)
+        {
+            if (cantidadEstudiantes > 50)
+            {
+                return precioUnit * 0.85; // 15% de descuento
+            }
+            else
+            {
+                return precioUnit;
+            }
+        }
+    }
+}
